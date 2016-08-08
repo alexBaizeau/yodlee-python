acb52ce4789ff9adff3cb8bcbdf1282c5a8f7c67
Alex Baizeau <abaizeau@freshbooks.com>
2016-08-08 10:51:47 -0400

    Play with fastlink a bit

--- test_api.py
+++ test_api.py
@@ -23,3 +23,33 @@ if __name__ == '__main__':
     for account in accounts['account']:
         print account['id']
         print account['accountName']
+
+    provider_accounts = user.get_provider_accounts()
+    print provider_accounts
+    account_to_refresh = provider_accounts[0]['id']
+    account_to_edit = provider_accounts[1]['id']
+
+    user.get_provider_account(account_to_refresh)
+
+    # When a user exits out of Fastlink they get redirected here.
+    callback_url = 'http://requestb.in/1c12mkr1'
+
+    refresh_account_extra_params = {
+        'siteAccountId':  account_to_refresh,
+        'flow': 'refresh',
+        'callback': callback_url
+    }
+
+    add_account_extra_params = {
+        'callback': callback_url
+    }
+
+    edit_account_extra_params = {
+        'siteAccountId':  account_to_refresh,
+        'flow': 'edit',
+        'callback': callback_url
+    }
+
+    user.get_fastlink_iframe_link(add_account_extra_params)
+    user.get_fastlink_iframe_link(refresh_account_extra_params)
+    user.get_fastlink_iframe_link(edit_account_extra_params)
--- yodlee/user.py
+++ yodlee/user.py
@@ -1,4 +1,20 @@
 import requests
+from jinja2 import Template
+import webbrowser
+import os
+from urlparse import parse_qs
+import urllib
+
+HTML="""<div class='center processText'>Processing...</div>
+<div>
+<form action='{url}' method='post' id='rsessionPost'>
+    RSession : <input type='text' name='rsession' placeholder='rsession' value='{rsession}' id='rsession'/><br/>
+    FinappId : <input type='text' name='app' placeholder='App' value='{app}' id='finappId'/><br/>
+    Redirect : <input type='text' name='redirectReq' placeholder='true/false' value='true'/><br/>
+    Token : <input type='text' name='token' placeholder='token' value='{token}' id='token'/><br/>
+    Extra Params : <input type='text' name='extraParams' placeholer='Extra Params' value='{extra_params}' id='extraParams'/><br/>
+</form></div>
+<script>document.getElementById('rsessionPost').submit();</script>"""
 
 
 class User(object):
@@ -31,3 +47,38 @@ class User(object):
         url = "{}/v1/accounts".format(self.cobrand.host)
         response = requests.get(url, headers=self.build_auth_headers())
         return response.json()
+
+    def get_provider_accounts(self):
+
+        url = "{}/v1/providers/providerAccounts".format(self.cobrand.host)
+        response = requests.get(url, headers=self.build_auth_headers())
+        return response.json()['providerAccount']
+
+    def get_provider_account(self, account_id):
+
+        url = "{}/v1/providers/providerAccounts/{}?include=credentials".format(self.cobrand.host, account_id)
+        response = requests.get(url, headers=self.build_auth_headers())
+
+
+    def get_fastlink_iframe_link(self, extra_params):
+
+        token_url = "{}/v1/providers/token".format(self.cobrand.host)
+
+        response = requests.get(token_url, headers=self.build_auth_headers()).json()
+        params = parse_qs(response['parameters'])
+        payload = {
+            'app': params['app'][0],
+            'redirectReq': params['redirectReq'][0],
+            'token': params['token'][0],
+            'rsession': params['rsession'][0],
+            'url': response['url'],
+            'extra_params': urllib.urlencode(extra_params)
+        }
+
+        html_content = HTML.format(**payload)
+        fobj = open("post.html", "wb")
+        fobj.write(html_content.encode('utf-8'))
+        fobj.close()
+        abs_file_path = os.path.abspath(fobj.name)
+        webbrowser.open("file://"+abs_file_path)
+        print payload
